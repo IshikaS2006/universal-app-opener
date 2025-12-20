dd189af41fae65b15edefaf4a048462ad1b46d40
IshikaS2006 <ishika.singh24@st.niituniversity.in>
2025-12-21 01:29:45 +0530

    feat(twitter): add profile deep link support for twitter/x

---
+++ packages/core/src/platforms/twitter.ts
@@ -0,0 +1,45 @@
+import { DeepLinkHandler } from '../types';
+
+export const twitterHandler: DeepLinkHandler = {
+  match: (url) =>
+    url.match(/(?:twitter\.com|x\.com)\/([^/?]+)/),
+
+  build: (webUrl, match) => {
+    if (!match || !match[1]) {
+      return null;
+    }
+
+    const username = match[1].toLowerCase();
+
+    const reservedPaths = new Set([
+      'home',
+      'explore',
+      'login',
+      'i',
+      'intent',
+      'notifications',
+      'messages',
+      'settings',
+    ]);
+
+    if (reservedPaths.has(username)) {
+      return null;
+    }
+
+    const deepLinks = {
+      default: {
+        ios: `twitter://user?screen_name=${username}`,
+        android: `intent://user?screen_name=${username}#Intent;scheme=twitter;package=com.twitter.android;end`,
+      },
+    };
+
+    const config = deepLinks.default;
+
+    return {
+      webUrl,
+      ios: config.ios,
+      android: config.android,
+      platform: 'twitter',
+    };
+  },
+};
